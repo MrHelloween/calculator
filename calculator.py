3e733e4f036d290a69ca3ed2d82a9ff814eced5f
MrHelloween <mrl189626@gmail.com>
2023-06-30 15:37:04 +0200

    addition of functions of division, multiplication, subtraction

--- calculator.py
+++ calculator.py
@@ -1,2 +1,16 @@
-def plus(a, b):
+def addition(a, b):
     return a + b
+
+
+def subtraction(a, b):
+    return a - b
+
+
+def multiplication(a, b):
+    return a * b
+
+
+def division(a, b):
+    return a / b
+
+
