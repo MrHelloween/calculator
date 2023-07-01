cb90505c76ef58d75cef01096e8f90fa5ad3115b
MrHelloween <mrl189626@gmail.com>
2023-07-01 09:44:03 +0200

    functions were collected into the main function

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import calculator
 
 
+def main():
+    number_menu = menu()
+    under_the_menu_number = under_the_menu_calculator(number_menu)
+    test(under_the_menu_number)
+
+
 def menu():
     print('''1. calculator \n2. Exit''')
 
     number_menu = int(input('Choose a number: '))
-    if number_menu == 1:
-        under_the_menu_calculator(number_menu)
-    elif number_menu == 2:
+    if number_menu == 2:
         print('The calculator has finished its work')
+    else:
+        return number_menu
 
 
 def under_the_menu_calculator(number_menu):
@@ -24,21 +30,25 @@ def under_the_menu_calculator(number_menu):
         ''')
 
     under_the_menu_number = int(input('Select an operation: '))
+    return under_the_menu_number
+
+
+def test(num):
     exit = ''
-    if under_the_menu_number == 6:
+    if num == 6:
         while exit.lower() != 't':
             a = int(input('Enter number: '))
-            result = operation(under_the_menu_number, a, )
+            result = operation(num, a, )
             print(f'Result operation: {result:,.2f}')
             exit = input('if you want to exit enter T/t if not F/f: ')
         under_the_menu_calculator(1)
-    elif under_the_menu_number == 7:
+    elif num == 7:
         menu()
     else:
         while exit.lower() != 't':
             a = int(input('Enter the first number: '))
             b = int(input('Enter the second digit: '))
-            result = operation(under_the_menu_number, a, b)
+            result = operation(num, a, b)
             print(f'Result operation: {result:,.2f}')
             exit = input('if you want to exit enter T/t if not F/f: ')
         under_the_menu_calculator(1)
