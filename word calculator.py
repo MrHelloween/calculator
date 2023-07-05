f3063976c7fe8aaef9ce90e946d554e9f788a382
MrHelloween <mrl189626@gmail.com>
2023-07-05 13:46:57 +0200

    added function get_words which breaks the test into words and cleans from symbols

--- word calculator.py
+++ word calculator.py
@@ -1,13 +1,29 @@
-
-
 def main():
     text = get_text()
+    get_words(text)
+
 
 def get_text():
-    text = open('text.txt','r')
+    text = open('text.txt', 'r')
     text_line = text.read()
     text.close()
-    return  text_line
+    return text_line
+
+
+def get_words(text):
+    list_words = text.split()
+    delete_back = ['.', ',', '?', ':', '!', ')','"']
+    delete_frond = ['(', '"']
+    new_list = []
+    for i in list_words:
+        if i[-1] in delete_back:
+            new_list.append(i[:-1])
+        elif i[0] in delete_frond:
+            new_list.append(i[1:])
+        else:
+            new_list.append(i)
+    print(new_list)
+
 
 if __name__ == '__main__':
     main()
