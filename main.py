4cdfef4659dc86eb7ea687471ed5d04705c1a729
MrHelloween <mrl189626@gmail.com>
2023-07-18 18:39:07 +0200

    fixed bugs #2 and  functionality extension

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import calculator
 import BMI
 import Word_count
 
+
 def main():
     number_menu = menu()
     under_the_menu_number = under_the_menu_calculator(number_menu)
@@ -33,12 +34,12 @@ def under_the_menu_calculator(number_menu):
         return under_the_menu_number
     elif number_menu == 2:
 
-
-        return 3
+        return 2
     elif number_menu == 3:
         print('''1. File with text \n2. Enter text''')
         num_menu = int(input('Enter num: '))
         count_words(num_menu)
+        return  None
 
 def test(num):
     try:
@@ -53,10 +54,13 @@ def test(num):
         elif num == 7:
             menu()
         elif num == 2:
-            weight = int(input('Enter weight (in kilograms): '))
-            height = int(input('Enter height (in centimeters): '))
-            result = BMI.bmi(weight,height)
-            print(f'BMI: {result}')
+            while exit.lower() !='t':
+                weight = int(input('Enter weight (in kilograms): '))
+                height = int(input('Enter height (in centimeters): '))
+                result = BMI.bmi(weight, height)
+                print(f'BMI: {result}')
+                exit = input('if you want to exit enter T/t if not F/f: ')
+            main()
         else:
             while exit.lower() != 't':
                 a = int(input('Enter the first number: '))
@@ -88,6 +92,7 @@ def operation(num, a, b=None):
     except:
         print('Error')
 
+
 def count_words(num_menu):
     if num_menu == 1:
         text_txt = input('Enter name file with text: ')
@@ -102,6 +107,7 @@ def count_words(num_menu):
         result = Word_count.get_the_word_count(list_words_count)
         filter_words_count = Word_count.filter(result)
         print(filter_words_count)
+
 
 if __name__ == '__main__':
     main()
